a2c1ac25dfc0e267100e4be70ecf04e7a9b44b87
Mamad Purbo <m.purbo@gmail.com>
2014-08-27 23:30:12 +0900

    line draw test & scale gesture experiment

--- app/src/main/java/org/purbo/seamallowance/RulerView.java
+++ app/src/main/java/org/purbo/seamallowance/RulerView.java
@@ -5,12 +5,16 @@ import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
 
 /**
  * Created by purbo on 8/20/14.
  */
 public class RulerView extends View {
+
+    private static final int INVALID_POINTER_ID = -1;
 
     float firstUnitMin;
     float firstUnitMax;
@@ -19,6 +23,17 @@ public class RulerView extends View {
 
     private int backgroundColor;
     private int foregroundColor;
+
+    //http://stackoverflow.com/questions/5790503/can-we-use-scale-gesture-detector-for-pinch-zoom-in-android
+    //http://www.zdnet.com/blog/burnette/how-to-use-multi-touch-in-android-2-part-6-implementing-the-pinch-zoom-gesture/1847
+
+    private ScaleGestureDetector scaleDetector;
+    private float scaleFactor = 1.f;
+    private float posX;
+    private float posY;
+    private float lastTouchX;
+    private float lastTouchY;
+    private int activePointerId = INVALID_POINTER_ID;
 
     public RulerView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -35,17 +50,93 @@ public class RulerView extends View {
         } finally {
             a.recycle();
         }
+
+        scaleDetector = new ScaleGestureDetector(context, new ScaleListener());
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+        canvas.save();
+        canvas.translate(posX, posY);
+        canvas.scale(scaleFactor, scaleFactor);
+
         Paint paint = new Paint();
         paint.setColor(foregroundColor);
 
-        //canvas.drawLine();
-        //this.getMeasuredWidth();
+        //canvas.drawLine(0, getMeasuredHeight()/2, getMeasuredWidth(), getMeasuredHeight()/2, paint);
+        canvas.drawLine(0, 0, getMeasuredWidth(), getMeasuredHeight(), paint);
+
+        canvas.restore();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        // Let the ScaleGestureDetector inspect all events.
+        scaleDetector.onTouchEvent(ev);
+
+        final int action = ev.getAction();
+        switch (action & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN: {
+                final float x = ev.getX();
+                final float y = ev.getY();
+
+                lastTouchX = x;
+                lastTouchY = y;
+                activePointerId = ev.getPointerId(0);
+                break;
+            }
+
+            case MotionEvent.ACTION_MOVE: {
+                final int pointerIndex = ev.findPointerIndex(activePointerId);
+                final float x = ev.getX(pointerIndex);
+                final float y = ev.getY(pointerIndex);
+
+                // Only move if the ScaleGestureDetector isn't processing a gesture.
+                if (!scaleDetector.isInProgress()) {
+                    final float dx = x - lastTouchX;
+                    final float dy = y - lastTouchY;
+
+                    posX += dx;
+                    posY += dy;
+
+                    invalidate();
+                }
+
+                lastTouchX = x;
+                lastTouchY = y;
+
+                break;
+            }
+
+            case MotionEvent.ACTION_UP: {
+                activePointerId = INVALID_POINTER_ID;
+                break;
+            }
+
+            case MotionEvent.ACTION_CANCEL: {
+                activePointerId = INVALID_POINTER_ID;
+                break;
+            }
+
+            case MotionEvent.ACTION_POINTER_UP: {
+                final int pointerIndex = (ev.getAction() & MotionEvent.ACTION_POINTER_INDEX_MASK)
+                        >> MotionEvent.ACTION_POINTER_INDEX_SHIFT;
+                final int pointerId = ev.getPointerId(pointerIndex);
+                if (pointerId == activePointerId) {
+                    // This was our active pointer going up. Choose a new
+                    // active pointer and adjust accordingly.
+                    final int newPointerIndex = pointerIndex == 0 ? 1 : 0;
+                    lastTouchX = ev.getX(newPointerIndex);
+                    lastTouchY = ev.getY(newPointerIndex);
+                    activePointerId = ev.getPointerId(newPointerIndex);
+                }
+                break;
+            }
+        }
+
+        return true;
     }
 
     public float getFirstUnitMin() {
@@ -106,5 +197,18 @@ public class RulerView extends View {
         this.foregroundColor = foregroundColor;
         invalidate();
         requestLayout();
+    }
+
+    private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+        @Override
+        public boolean onScale(ScaleGestureDetector detector) {
+            scaleFactor *= detector.getScaleFactor();
+
+            // Don't let the object get too small or too large.
+            scaleFactor = Math.max(0.1f, Math.min(scaleFactor, 10.0f));
+
+            invalidate();
+            return true;
+        }
     }
 }
